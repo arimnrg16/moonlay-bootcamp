cea249f5be5cec2c44bcf1d643add28440c8570d
arinrg16 <arimnrg16@gmail.com>
2019-06-12 13:11:13 +0700

    moonlay-bootcamp

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using LearnCSharp.Vehicles;
+using System;
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicleBuilder = new VehicleBuilder();
+
+            Vehicle avanza = vehicleBuilder
+                .WithManufacture("Toyota")
+                .WithType("Avanza")
+                .HavingDoors(5)
+                .HavingWheels(4)
+                .HavingWheelRadius(18.5);
+
+            Console.WriteLine(avanza);
+            Console.ReadKey();
+            Console.WriteLine();
+
+            avanza = vehicleBuilder.Move(10);
+            Console.WriteLine(avanza);
+            Console.ReadKey();
+            Console.WriteLine();
+
+            avanza = vehicleBuilder.Stop();
+            Console.WriteLine(avanza);
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LearnCSharp/SpareParts/speedometer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnCSharp.SpareParts
+{
+    public class Speedometer
+    {
+        public double Speed { get; set; }
+
+        public double Range { get; set; }
+    }
+}
